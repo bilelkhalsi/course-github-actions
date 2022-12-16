3a4e4e9a1bd1d96f1793bc6b4d02a1cc65eb0b08
Bilel Khalsi <bilel_khalsi@live.fr>
2022-12-16 21:02:57 +0000

    Swagger module integrated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Print Web page API')
+    .setDescription('Print web page and generate PDF API')
+    .setVersion('1.0')
+    .addTag('Audit')
+    .addTag('Print')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
+
+
