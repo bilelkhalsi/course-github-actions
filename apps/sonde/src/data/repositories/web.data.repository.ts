6962f6fb4e400f146eb45ac8ba9a17c92d83ed1b
Bilel Khalsi <bilel_khalsi@live.fr>
2022-12-18 02:35:09 +0000

    web data respository implemented

--- apps/sonde/src/data/repositories/web.data.repository.ts
+++ apps/sonde/src/data/repositories/web.data.repository.ts
@@ -1,25 +1,58 @@
-import { Injectable } from "@nestjs/common";
-import { PuppeteerNode } from "puppeteer";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PuppeteerNode, Browser } from "puppeteer";
 import { WebValueMono, WebValueSelector, WebValueSet } from "../model/web.value.model";
 
 /**
  * Web data repository
  */
 @Injectable()
-export class WebDataRepository {
+export class WebDataRepository implements OnModuleInit, OnModuleDestroy {
+
+    private browser : Browser;
 
     constructor(private readonly puppeteer: PuppeteerNode){}
 
+    async onModuleInit() {
+        this.browser = await this.puppeteer.launch();
+    }
+
     async selectOne(selector: WebValueSelector): Promise<WebValueMono> {
-        const browser = await this.puppeteer.launch();
-        const page = await browser.newPage();
+        const page = await this.browser.newPage();
         await page.goto(selector.source);
         await page.waitForSelector(selector.selector);
-        return Promise.reject();
+        return page.evaluate(selector => {
+            const elem = document.querySelector(selector);
+            return (elem && elem.textContent)? elem.textContent : '';
+        }, selector.selector).then(value => {
+            return {
+                source: selector.source,
+                selector: selector.selector,
+                timestamp: new Date(),
+                value
+            };
+        });
     }  
 
     async selectAll(selector: WebValueSelector): Promise<WebValueSet> {
-        return Promise.reject();
+        const page = await this.browser.newPage();
+        await page.goto(selector.source);
+        await page.waitForSelector(selector.selector);
+        return page.evaluate(selector => {
+            return [...document.querySelectorAll(selector)].map(elem => {
+                return (elem && elem.textContent)? elem.textContent : '';
+            });
+        }, selector.selector).then(value => {
+            return {
+                source: selector.source,
+                selector: selector.selector,
+                timestamp: new Date(),
+                value
+            };
+        });
     }  
+    
+    async onModuleDestroy() {
+        await this.browser.close();
+    }
 
 }
