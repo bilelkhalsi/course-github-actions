871c0c76bdb4cd2f785421ffad7cd86cb2a4e1b1
Bilel Khalsi <bilel_khalsi@live.fr>
2022-12-22 07:05:45 +0000

    Get schedules from config data

--- apps/dispatcher/src/schedule.module.ts
+++ apps/dispatcher/src/schedule.module.ts
@@ -1,11 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as fs from 'fs/promises';
 import { ScheduleController } from './api/schedule.controller';
+import { ScheduleRepository } from './data/repositories/schedule.repository';
 import { WebDataClient } from './data/repositories/web.data.client';
 import { ScheduleService } from './service/schedule.service';
 
+function scheduleRepositoryFactory(configService: ConfigService): Promise<ScheduleRepository>{
+  return fs.readFile(configService.get('SCHEDULES_FILE_PATH'), 'utf8').then(raw => {
+    Logger.debug('Loading schedules json file :', raw);
+    return new ScheduleRepository(raw);
+  }).catch(error=>{
+    return new ScheduleRepository('{"data":[]}');
+  })
+}
+
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [ScheduleController],
-  providers: [WebDataClient, ScheduleService],
+  providers: [
+    WebDataClient,
+    ScheduleService,
+    {
+      provide: ScheduleRepository,
+      useFactory: scheduleRepositoryFactory,
+      inject: [ConfigService]
+    }
+],
 })
 export class ScheduleModule {}
--- apps/dispatcher/src/service/schedule.service.ts
+++ apps/dispatcher/src/service/schedule.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { SchedulesResult } from "../data/model/schedule.model";
+import { ScheduleRepository } from "../data/repositories/schedule.repository";
 
 @Injectable()
 export class ScheduleService {
 
+    constructor(private readonly scheduleRepository: ScheduleRepository){}
+
     schedule(schedule: string): SchedulesResult {
+        const schedules =  this.scheduleRepository.findBySchedule(schedule);
         return null;
     }
     
